58c7c700c792a8b32ed54a061d281714505aa8d3
xlb233 <xyc121305596@gmail.com>
2021-03-23 16:16:26 +0800

    项目初始化，Home公共组件和样式编写完毕

--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,12 +1,25 @@
 // Home公共组件的入口
+
 import React from 'react';
+import { NavLink } from 'react-router-dom'  // 利用 NavLink 组件进行路由跳转
 import { renderRoutes } from "react-router-config";
+// 引入在./style.js中生成的样式
+import { Top, Tab, TabItem } from "./style"
 
 function Home (props) {
   const { route } = props
   return (
     <div>
-      <div>Home</div>
+      <Top>
+        <span className="iconfont menu">&#xe65c;</span>
+        <span className="title">Web App</span>
+        <span className="iconfont search">&#xe62b;</span>
+      </Top>
+      <Tab>
+        <NavLink to="/recommend" activeClassName="selected"><TabItem><span > 推荐 </span></TabItem></NavLink>
+        <NavLink to="/singers" activeClassName="selected"><TabItem><span > 歌手 </span></TabItem></NavLink>
+        <NavLink to="/rank" activeClassName="selected"><TabItem><span > 排行榜 </span></TabItem></NavLink>
+      </Tab>
       { renderRoutes(route.routes) }
     </div>
   )
---
+++ src/application/Home/style.js
@@ -0,0 +1,51 @@
+// Home组件的样式文件，将在./index.js中被引用
+import styled from 'styled-components'
+// 引入组件全局样式, 并使用styled-components生成样式
+import style from '../../assets/global-style'
+
+// 顶栏样式
+export const Top = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 5px 10px;
+  background: ${style ["theme-color"]};
+  &>span {
+    line-height: 40px;
+    color: #f1f1f1;
+    font-size: 20px;
+    &.iconfont {
+      font-size: 25px;
+    }
+  }
+`
+// 标签栏样式
+export const Tab = styled.div`
+  height: 44px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  background: ${style ["theme-color"]};
+  a {
+    flex: 1;
+    padding: 2px 0;
+    font-size: 14px;
+    color: #e4e4e4;
+    &.selected {
+      span {
+        padding: 3px 0;
+        font-weight: 700;
+        color: #f1f1f1;
+        border-bottom: 2px solid #f1f1f1;
+      }
+    }
+  }
+`
+// 标签样式
+export const TabItem = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
