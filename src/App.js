407a7f8d88cc36da4a50d18919939607a912bed9
xlb233 <xyc121305596@gmail.com>
2021-03-23 15:12:04 +0800

    项目初始化，引入styled-component生成样式文件完成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// 引入./style.js通过styled-components包生成的的全局样式
+import {GlobalStyle} from "./style";
+// 引入字体样式，同样是styled-components包生成的
+import { IconStyle } from "./assets/iconfont/iconfont";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
+      <i className="iconfont">&#xe62b;</i>
     </div>
   );
 }
